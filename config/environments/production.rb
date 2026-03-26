bc3d638bcd8f15a6043c32473c1c7fe100fed3e0
Salod4 <151103696+Salod4@users.noreply.github.com>
2026-03-26 10:20:44 -0600

    Cambios para mail en production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,15 +57,15 @@ Rails.application.configure do
   # Set host to be used by links generated in mailer templates.
   config.action_mailer.default_url_options = { host: ENV.fetch("APP_HOST", "example.com") }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: "smtp.sendgrid.net",
-    port: 587,
-    domain: ENV.fetch("MAIL_DOMAIN", "example.com"),
-    user_name: ENV.fetch("SENDGRID_USERNAME", "apikey"),
-    password: ENV.fetch("SENDGRID_API_KEY", nil),
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
+config.action_mailer.smtp_settings = {
+  address: "smtp.resend.com",
+  port: 587,
+  domain: ENV.fetch("MAIL_DOMAIN", "example.com"),
+  user_name: "resend",
+  password: ENV.fetch("RESEND_API_KEY", nil),
+  authentication: :plain,
+  enable_starttls_auto: true
+}
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
